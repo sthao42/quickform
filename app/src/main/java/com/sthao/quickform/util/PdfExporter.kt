abdb9442100a666095e043d00cd7dee0afeb9671
sthao42 <sirgrimz@proton.me>
2025-07-04 14:11:31 -0500

    change attached image in pdf to 1 column

--- app/src/main/java/com/sthao/quickform/util/PdfExporter.kt
+++ app/src/main/java/com/sthao/quickform/util/PdfExporter.kt
@@ -4,7 +4,6 @@ import android.content.ContentValues
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.Matrix
 import android.graphics.Paint
 import android.graphics.RectF
 import android.graphics.Typeface
